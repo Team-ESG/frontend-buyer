59b952f4f3073f8f93db04386efb70bdd1ffe8a6
js1022003 <js1022003@ajou.ac.kr>
2023-03-29 18:32:44 +0900

    [chore] eslint prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,48 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'prettier',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // import 'react'가 필요 없음
+    'react/react-in-jsx-scope': 'off',
+    // import 순서
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'type'],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroupsExcludedImportTypes: ['builtin'],
+        warnOnUnassignedImports: true,
+        'newlines-between': 'always',
+      },
+    ],
+  },
 };
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import {Button, Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { Button, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-function Login({navigation}: any): JSX.Element {
+function Login({ navigation }: any): JSX.Element {
   return (
     <SafeAreaView
-      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+    >
       <Text>Login</Text>
       <Button
         title="Signup"
