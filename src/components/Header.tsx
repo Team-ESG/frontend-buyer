31acf002973f12bb6afd01167e040169160c4a00
js1022003 <js1022003@ajou.ac.kr>
2023-04-29 22:32:46 +0900

    Design: 헤더, 찜, 가게 상세 페이지 디자인 초안

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,44 @@
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
 
-function Header(): JSX.Element {
-  return <Text>Header</Text>;
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default function Header(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Text style={styles.locationText}>영통구</Text>
+        <Icon name="keyboard-arrow-down" size={25} color="#333" />
+      </View>
+      <Pressable
+        onPress={() => {
+          console.log('Search');
+        }}
+      >
+        <Icon name="search" size={25} color="#333" />
+      </Pressable>
+    </View>
+  );
 }
 
-export default Header;
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  leftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  locationText: {
+    fontSize: 19,
+    fontWeight: 'bold',
+    color: 'black',
+    marginRight: 5,
+  },
+  rightContainer: {},
+});
